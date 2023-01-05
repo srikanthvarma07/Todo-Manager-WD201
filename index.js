95fd469da853e5ce2eb3cf84a61bc7397bfbc3d5
srikanth varma <75570276+srikanthvarma07@users.noreply.github.com>
2023-01-05 10:09:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const app = require('./app');
-app.listen(3000, () => {
-    console.log("Started express server at port 3000")
-})
+const app = require("./app.js");
+const express = require("express");
+const path = require("path");
+
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Started express server at port 3000");
+});
